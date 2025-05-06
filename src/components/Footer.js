b2a65b2a5ca21e955420b524e104d00d1c54a84b
Rohit Kumar <rohitkr.7555@gmail.com>
2025-05-06 18:26:54 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/Logo-removebg-preview.png"
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -14,7 +14,7 @@ export const Footer = () => {
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          <Col size={16} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
             <a href="http://www.linkedin.com/in/-rohitkumar"><img src={navIcon1} alt="Icon" /></a>
 
